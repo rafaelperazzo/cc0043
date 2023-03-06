c03518e6bce766ad3ff3909854349f8542bcf668
Rafael Perazzo <rafaelperazzo@gmail.com>
2023-03-06 15:24:44 -0300

    Incluido projeto react native

---
+++ projetoMob/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Olá mundo</Text>
+    </View>
+  );
+}
+
+export default App;
